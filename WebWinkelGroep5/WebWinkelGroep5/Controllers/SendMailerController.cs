8d5c996629f189235607200eec85011cdf42d564
Murat <14083132@student.hhs.nl>
2015-03-26 16:12:03 +0100

    mail functie uitgebouwd en verbeterd

--- WebWinkelGroep5/WebWinkelGroep5/Controllers/SendMailerController.cs
+++ WebWinkelGroep5/WebWinkelGroep5/Controllers/SendMailerController.cs
@@ -21,9 +21,29 @@ namespace WebWinkelGroep5.Controllers
         {
             if (ModelState.IsValid)
             {
+                //SmtpClient smtpClient = new SmtpClient("smtp.googlemail.com", 465);
+                SmtpClient smtpClient = new SmtpClient();
+                smtpClient.Host = "smtp.googlemail.com";
+                smtpClient.Port = 587;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new System.Net.NetworkCredential("wooods-info@compuboot.in", "Wooods123");
+                smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtpClient.EnableSsl = true;
+                MailMessage mail = new MailMessage();
+
+                //Setting From , To and CC
+                mail.From = new MailAddress("wooods-info@compuboot.in", "Wooods BV.");
+                mail.To.Add(_objModelMail.To);
+                mail.Subject = _objModelMail.Subject;
+                string Body = _objModelMail.Body;
+                mail.Body = Body;
+                mail.IsBodyHtml = true;
+
+
+                /*smtpClient.Send(mail);
                 MailMessage mail = new MailMessage();
                 mail.To.Add(_objModelMail.To);
-                mail.From = new MailAddress(_objModelMail.From);
+                //mail.From = new MailAddress(_objModelMail.From);
                 mail.Subject = _objModelMail.Subject;
                 string Body = _objModelMail.Body;
                 mail.Body = Body;
@@ -33,10 +53,49 @@ namespace WebWinkelGroep5.Controllers
                 smtp.Port = 587;
                 smtp.UseDefaultCredentials = false;
                 smtp.Credentials = new System.Net.NetworkCredential
-                ("username", "password");// Enter seders User name and password
-                smtp.EnableSsl = true;
-                smtp.Send(mail);
-                return View("mailtest   ", _objModelMail);
+                ("wooods@gmail.com", "Wooods1234");// Enter seders User name and password
+                smtp.EnableSsl = true;*/
+                smtpClient.Send(mail);
+                return View("mailtest", _objModelMail);
+                
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact";
+
+            return View();
+        }
+        [HttpPost]
+        public ViewResult Contact(WebWinkelGroep5.Models.MailModel _objModelMail)
+        {
+            if (ModelState.IsValid)
+            {
+                SmtpClient smtpClient = new SmtpClient();
+                smtpClient.Host = "smtp.googlemail.com";
+                smtpClient.Port = 587;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new System.Net.NetworkCredential("wooods-info@compuboot.in", "Wooods123");
+                smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtpClient.EnableSsl = true;
+                MailMessage mail = new MailMessage();
+
+                //Setting From , To and CC
+                mail.From = new MailAddress(_objModelMail.From);
+                mail.To.Add("wooods-info@compuboot.in");
+                mail.Subject = _objModelMail.Subject;
+                string Body = _objModelMail.Body;
+                mail.Body = mail.From + "<br><br>" + Body;
+                mail.IsBodyHtml = true;
+
+                smtpClient.Send(mail);
+                return View("Contact", _objModelMail);
+
             }
             else
             {
